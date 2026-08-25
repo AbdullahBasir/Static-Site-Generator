fd587621cea6306caa205cf59de43ecdb6c72f35
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-25 15:29:11 +0400

    Added unit tests

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A Boot.dev guided project on building a static site generator.
 Python server: ```python3 -m http.server 8888```
 
 CSS link to HTML: ```<link rel="stylesheet" href="/styles.css" />```
+
+Unittest a directory(src): ```python3 -m unittest discover -s src```
---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a url link", TextType.LINK, "https://www.boot.dev")
+        node2 = TextNode("This is a url link", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+    def test_url_default_none(self):
+        node = TextNode("Boot.dev", TextType.PLAIN_TEXT)
+        self.assertIsNone(node.url)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
