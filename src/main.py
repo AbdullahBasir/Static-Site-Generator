dcc026b7c1a5a88b5bea2559eed3fe248cceb52e
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-25 11:19:00 +0400

    Added a Text type class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    link_node = TextNode("click here", TextType.LINK, "https://www.boot.dev")
+    print(link_node)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return NotImplemented
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
