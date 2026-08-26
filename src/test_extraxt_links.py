a457b50d0808483e18ce50a4152170bdbb8e1da3
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-26 18:58:55 +0400

    added link extraction from test with regex

---
+++ src/test_extraxt_links.py
@@ -0,0 +1,16 @@
+import unittest
+from extract_links import extract_markdown_images, extract_markdown_links
+from textnode import TextNode, TextType
+
+class TestExtract(unittest.TestCase):
+    def test_extract_img(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(result, expected)
+
+    def test_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(result, expected)
