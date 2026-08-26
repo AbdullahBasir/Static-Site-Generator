92f756974cd79209071a9dd04e3ea8f42a129f9a
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-26 12:25:42 +0400

    Added the text node to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -15,6 +15,20 @@ class TestTextNode(unittest.TestCase):
     def test_url_default_none(self):
         node = TextNode("Boot.dev", TextType.PLAIN_TEXT)
         self.assertIsNone(node.url)
-        
+
+    #text_node_to_html_node function tests
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("Click here", TextType.LINK, "https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click here")
+        self.assertEqual(html_node.props, {"href": "https://boot.dev"})
+
 if __name__ == "__main__":
     unittest.main()
