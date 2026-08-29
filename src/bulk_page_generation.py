0dd78f6c7962e79c966f5c6fa592c741bc369ff1
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-29 17:17:56 +0400

    Copied pages to docs to use in github pages

--- src/bulk_page_generation.py
+++ src/bulk_page_generation.py
@@ -1,9 +1,8 @@
-import os
 from markdown_to_html import markdown_to_html_node
 from title import extract_title
 from pathlib import Path
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     content_path = Path(dir_path_content)
     dest_path = Path(dest_dir_path)
     template_path = Path(template_path)
@@ -19,10 +18,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             
             template_content = template_content.replace("{{ Title }}", html_title)
             template_content = template_content.replace("{{ Content }}", html_content)
+            template_content = template_content.replace('href="/', f'href="{base_path}')
+            template_content = template_content.replace('src="/', f'src="{base_path}')
 
             output_file = dest_path / item.relative_to(content_path).with_suffix(".html")
             output_file.parent.mkdir(parents=True, exist_ok=True)
             output_file.write_text(template_content)
 
         elif item.is_dir():
-            generate_pages_recursive(item, template_path, dest_path / item.name)
+            generate_pages_recursive(item, template_path, dest_path / item.name, base_path)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 from copystatic import static_to_public
-import os
 from bulk_page_generation import generate_pages_recursive
+import sys
+import os
 
+
+base_path = sys.argv[1] if len(sys.argv) > 1 else "/"
 project_dir = os.getcwd()
 static_path = os.path.join(project_dir, "static")
-public_path = os.path.join(project_dir, "public")
+docs_path = os.path.join(project_dir, "docs")
 content_path = os.path.join(project_dir, "content")
 template_path = os.path.join(project_dir, "template.html")
 
 def main():
 
-    static_to_public(static_path, public_path)
-    generate_pages_recursive(content_path, template_path, public_path)
+    static_to_public(static_path, docs_path)
+    generate_pages_recursive(content_path, template_path, docs_path, base_path)
 
 if __name__ == "__main__":
     main()
