5a7d060f9d0ff4293d285e7c4eb8d845b9b26c31
abdullahbasir <abdullah.sbasir@gmail.com>
2026-08-29 13:38:13 +0400

    Added a function that converts all files

---
+++ src/bulk_page_generation.py
@@ -0,0 +1,28 @@
+import os
+from markdown_to_html import markdown_to_html_node
+from title import extract_title
+from pathlib import Path
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    content_path = Path(dir_path_content)
+    dest_path = Path(dest_dir_path)
+    template_path = Path(template_path)
+
+    for item in content_path.iterdir():
+        if item.is_file() and item.suffix == ".md":
+            md_content = item.read_text()
+            template_content = template_path.read_text()
+            
+            node = markdown_to_html_node(md_content)
+            html_content = node.to_html()
+            html_title = extract_title(md_content)
+            
+            template_content = template_content.replace("{{ Title }}", html_title)
+            template_content = template_content.replace("{{ Content }}", html_content)
+
+            output_file = dest_path / item.relative_to(content_path).with_suffix(".html")
+            output_file.parent.mkdir(parents=True, exist_ok=True)
+            output_file.write_text(template_content)
+
+        elif item.is_dir():
+            generate_pages_recursive(item, template_path, dest_path / item.name)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from copystatic import static_to_public
 import os
-from page_generation import generate_page
+from bulk_page_generation import generate_pages_recursive
+
+project_dir = os.getcwd()
+static_path = os.path.join(project_dir, "static")
+public_path = os.path.join(project_dir, "public")
+content_path = os.path.join(project_dir, "content")
+template_path = os.path.join(project_dir, "template.html")
 
 def main():
 
-    project_dir = os.getcwd()
-    static_path = os.path.join(project_dir, "static")
-    public_path = os.path.join(project_dir, "public")
     static_to_public(static_path, public_path)
-
-    content_path = os.path.join(project_dir, "content")
-    template_path = os.path.join(project_dir, "template.html")
-    generate_page(content_path, template_path, public_path)
+    generate_pages_recursive(content_path, template_path, public_path)
 
 if __name__ == "__main__":
     main()
